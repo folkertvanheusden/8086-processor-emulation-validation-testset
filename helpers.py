0942032eb93daf61f2da5de031e550617bad07b5
Folkert van Heusden <mail@vanheusden.com>
2023-07-21 00:15:24 +0200

    set "Manufacturing Diagnostics port" ($0080) to 0 at start and to $ff if success

--- helpers.py
+++ helpers.py
@@ -9,12 +9,17 @@ def emit_header(fh):
     fh.write('\txor ax,ax\n')  # set si to 0
     fh.write('\tmov si,ax\n')
     fh.write('\n')
+    fh.write('\tout $80,al\n')  # set "Manufacturing Diagnostics port" to 0
+    fh.write('\n')
     fh.write('\tmov ss,ax\n')  # set stack segment to 0
     fh.write('\tmov ax,#$800\n')  # set stack pointer
     fh.write('\tmov sp,ax\n')  # set stack pointer
+
 
 def emit_tail(fh):
     # to let emulator know all was fine
     fh.write('\tmov ax,#$a5ee\n')
     fh.write('\tmov si,ax\n')
+    fh.write('\tmov al,#$ff\n')
+    fh.write('\tout $80,al\n')  # set "Manufacturing Diagnostics port" to ff
     fh.write('\thlt\n')

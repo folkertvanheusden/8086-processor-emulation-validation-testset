612c7ae948b3a8f4577fc61acbe458dfe039c384
Folkert van Heusden <mail@vanheusden.com>
2023-07-21 22:02:06 +0200

    configurable

---
+++ configuration.py
@@ -0,0 +1,2 @@
+# needs to be at least 0x100 (for a stack)
+program_offset = 0x100
--- helpers.py
+++ helpers.py
@@ -1,5 +1,7 @@
+from configuration import *
+
 def emit_header(fh):
-    fh.write('\torg $800\n')
+    fh.write(f'\torg ${program_offset:04X}\n')
     fh.write('\n')
 
     fh.write('\tcli\n')  # disable interrupts
@@ -12,7 +14,7 @@ def emit_header(fh):
     fh.write('\tout $80,al\n')  # set "Manufacturing Diagnostics port" to 0
     fh.write('\n')
     fh.write('\tmov ss,ax\n')  # set stack segment to 0
-    fh.write('\tmov ax,#$800\n')  # set stack pointer
+    fh.write(f'\tmov ax,#${program_offset:04X}\n')  # set stack pointer
     fh.write('\tmov sp,ax\n')  # set stack pointer
 
 

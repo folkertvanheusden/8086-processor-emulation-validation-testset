a68536d514b6d78df339bafda981c656fe27749c
folkert van heusden <mail@vanheusden.com>
2023-07-22 22:42:17 +0200

    type fix & show progress

--- run-test-pi86.sh
+++ run-test-pi86.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
 function function_generate() {
-	./generate
+	echo Generate test set assembly code...
+	./generate.sh
 
+	echo Translating assembly code to binary...
 	for i in test/*.asm
 	do
 		BASE=`echo $i | sed -e 's/.asm$//g'`
@@ -13,10 +15,12 @@ function function_generate() {
 
 # function_generate
 
+echo Compiling validation tool...
 cd pi86-verify
 mkdir build
 (cd build && cmake .. && make -j)
 
+echo Running test set...
 for i in ../test/*.bin
 do
 	echo -n "$i - "

7a94f85e568558d44b1bc513f2404e1838041dba
Folkert van Heusden <mail@vanheusden.com>
2023-07-23 15:04:33 +0100

    not only misc3 should handle trace-interrupt

--- helpers.py
+++ helpers.py
@@ -16,7 +16,15 @@ def emit_header(fh):
     fh.write('\tmov ss,ax\n')  # set stack segment to 0
     fh.write(f'\tmov ax,#${program_offset:04X}\n')  # set stack pointer
     fh.write('\tmov sp,ax\n')  # set stack pointer
-
+    fh.write('\n')
+    fh.write('\tmov ax,#intvec\n')  # make sure trace-int does not confuse anything
+    fh.write('\tmov word [4],ax\n')
+    fh.write('\txor ax,ax\n')
+    fh.write('\tmov word [6],ax\n')
+    fh.write('\tjmp init_continue\n')
+    fh.write('intvec:\n')
+    fh.write('\tiret\n')
+    fh.write('init_continue:\n')
 
 def emit_tail(fh):
     # to let emulator know all was fine

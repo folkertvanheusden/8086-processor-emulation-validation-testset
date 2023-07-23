c778aaa3b3b69c71178691373556b32b82b8319c
Folkert van Heusden <mail@vanheusden.com>
2023-07-23 09:44:13 +0100

    verify one asm

---
+++ test-one-pi86.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+BASE=`echo $1 | sed -e 's/.asm$//g'`
+
+as86 -0 -l ${BASE}.list -m -b ${BASE}.bin $1
+
+echo Compiling validation tool...
+(cd pi86-verify
+mkdir build
+cd build && cmake .. && make -j)
+
+./pi86-verify/build/verify-with-pi86 ${BASE}.bin ${BASE}.log
+
+if [ $? -ne 0 ] ; then
+	echo $i ${BASE}.log FAILED
+fi
